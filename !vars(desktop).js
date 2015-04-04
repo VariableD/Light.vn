f1b159ef23e2c305c1f6ac01d98731a84e7b362b
hsdk.bd <berserkd.dhsk@gmail.com>
2015-04-04 11:57:45 -0500

    update

--- !vars(desktop).js
+++ !vars(desktop).js
@@ -3,3 +3,8 @@
 var gAppType = "app(desktop)"; //dev, app(editor), app(desktop), app(mobile)
 
 var gAppDir = "./data_vn/";
+
+if ( true )
+{
+    gAppDir = "https://rawgithub.com/hsdk123/Light.vn/gh-pages/data_vn/";
+}
